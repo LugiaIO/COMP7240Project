390844edb1efd224d806d4fa501d3920a6772540
Donald <vesper.port@gmail.com>
2023-04-24 09:55:22 +0800

    Upload version

--- app.py
+++ app.py
@@ -167,4 +167,4 @@ def submitRating():
 
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 8080)), host="0.0.0.0", debug=True)
+    app.run(port=int(os.environ.get("PORT", 8081)), host="0.0.0.0", debug=True)
